4f9a4379841b7718430d5a24f9ed2e461fac4c36
Ash Madden <ashleymadden55@gmail.com>
2024-09-24 17:04:27 +1000

    Dynamic image import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Desserts from './components/Desserts';
+import Cart from './components/Cart';
+import dessertData from './data.json';
 import './App.css';
 
 function App() {
-	return <></>;
+	return (
+		<main>
+			<Desserts dessertData={dessertData} />
+			<Cart />
+		</main>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'red-hat-text': ['Red Hat Text', 'sans-serif'],
+			},
+			colors: {
+				red: '#C73B0F',
+				'rose-900': '#260F08',
+				'rose-500': '#87635A',
+				'rose-400': '#AD8A85',
+				'rose-300': '#CAAFA7',
+				'rose-100': '#F5EEEC',
+				'rose-50': '#FCF8F6',
+				green: '#1EA575',
+				black: '#000000',
+				white: 'FFFFFF',
+			},
+			screens: {
+				md: '768px',
+				lg: '1024px',
+			},
+		},
 	},
 	plugins: [],
 };
